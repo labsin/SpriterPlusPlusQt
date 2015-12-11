3c4b4193e65e10ca7614f4197f1c0c3edef1bcac
labsin <sam.sgrs@gmail.com>
2015-12-11 22:34:14 +0100

    Fix QML plugin in example
    Add CMake option to choose if you want the QML plugin build.
    Fixes the issue with the example and makes it valid for both.

--- example/main.cpp
+++ example/main.cpp
@@ -7,8 +7,10 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+#ifndef COMPILE_QML_PLUGIN
 	qmlRegisterType<QSprinterModel>("Spriter", 0, 1, "SpriterModel");
 	qmlRegisterType<QEntityInstance>("Spriter", 0, 1, "EntityInstance");
+#endif
     QQuickView view;
 	view.setResizeMode(QQuickView::SizeRootObjectToView);
 	view.setSource(QUrl::fromLocalFile("main.qml"));
